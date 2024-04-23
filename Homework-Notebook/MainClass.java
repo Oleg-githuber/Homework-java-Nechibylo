601c535f6a31df40f7c5829fb9dfaca11d3f5655
Oleg <oilminer@mail.ru>
2024-04-23 16:44:05 +0500

    Создание списка ноутбуков 3

--- Homework-Notebook/MainClass.java
+++ Homework-Notebook/MainClass.java
@@ -69,38 +69,52 @@ public class MainClass {
         filter.put(4, "Диагональ экрана");
         filter.put(5, "Модель");
 
-        createNotebookList();   // Создание списка ноутбуков
+        Map<Integer, Notebook> notebooks = createNotebookList();   // Создание списка ноутбуков
     }
 
     // Создание списка ноутбуков
-    public static void createNotebookList() {
+    public static Map<Integer, Notebook> createNotebookList() {
+        Map<Integer, Notebook> lst = new HashMap<>();
         Notebook notebook1 = new Notebook(4, 512, "Windows", "Чёрный", 
         14.5, "ASUS", "as1827");
+        lst.put(1, notebook1)
         Notebook notebook2 = new Notebook(8, 512, "Windows", "Чёрный", 
         15.0, "ASUS", "as1417");
+        lst.put(2, notebook2);
         Notebook notebook3 = new Notebook(32, 1024, "Windows", "Серый", 
         17.0, "ASUS", "as2518");
+        lst.put(3, notebook3);
 
         Notebook notebook4 = new Notebook(8, 512, "Без ОС", "Серый", 
         14.5, "ACER", "ar243");
+        lst.put(4, notebook4);
         Notebook notebook5 = new Notebook(16, 512, "Windows", "Чёрный", 
         15.0, "ACER", "ar118");
+        lst.put(5, notebook5);
         Notebook notebook6 = new Notebook(32, 2048, "Windows", "Белый", 
         15.0, "ACER", "ar553");
+        lst.put(6, notebook6);
 
         Notebook notebook7 = new Notebook(8, 256, "Dos", "Чёрный", 
         15.0, "LENOVO", "l5646");
+        lst.put(7, notebook7);
         Notebook notebook8 = new Notebook(8, 512, "Dos", "Чёрный", 
         17.0, "LENOVO", "l2426");
+        lst.put(8, notebook8);
         Notebook notebook9 = new Notebook(16, 512, "Windows", "Серый", 
         15.0, "LENOVO", "l5614");
+        lst.put(9, notebook9);
 
         Notebook notebook10 = new Notebook(4, 256, "Без ОС", "Серый", 
         15.0, "DEXP", "d145");
+        lst.put(10, notebook10);
         Notebook notebook11 = new Notebook(4, 256, "Без ОС", "Серый", 
         17.0, "DEXP", "d314");
+        lst.put(11, notebook11);
         Notebook notebook12 = new Notebook(8, 512, "Linux", "Чёрный", 
         15.0, "DEXP", "d777");
+        lst.put(12, notebook12);
+        return lst;
     }
 
 }
